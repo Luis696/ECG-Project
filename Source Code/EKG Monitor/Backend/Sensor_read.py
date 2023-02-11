6fda6b9cd833cadf1645bf2f0a8b29dadd4d12c4
Luis696 <Luiskre@googlemail.com>
2023-02-11 17:56:49 +0100

    Read Sensor Values
    Added a sub Thread to read the Sensor Values, and a Pipe to send the values to the updating Threads of the GUI

--- Source Code/EKG Monitor/Backend/Sensor_read.py
+++ Source Code/EKG Monitor/Backend/Sensor_read.py
@@ -1,18 +1,45 @@
-from random import randint
-import numpy as np
-from GUI.updateGUI import TIMER
-import time
 
-class Sensor:
-    def __init__(self, name_str):
-        self.name = name_str
+import struct
+import serial
+from PyQt5.QtCore import QObject, QThread, pyqtSignal, Qt
+
+
+
+class SensorBoard(QObject):
+    def __init__(self, name, com_port, baude_rate, num_sensors):
+        super().__init__()
+        self.name = str(name)
+        self.com_port = str(com_port)
+        self.baude_rate = int(baude_rate)
+        self.num_sensors = num_sensors
+        self.Serial_connection = serial.Serial(self.com_port, self.baude_rate, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+        self.data_pipe = None
 
     def print_properties(self):
-        print("Name:{name}".format(name=self.name))
+        print(f"Name: {self.name}")
+        print(f"COM-Port:  {self.com_port}")
+        print(f"Bauderate: {self.baude_rate}")
+        print(f"Number of Sensors enabled: {self.num_sensors}")  # TODO: Change to List of Sensor names
 
-    def read(self):
-        y = randint(1, 100)
-        return y
+    def read_serial_data(self):  # TODO: Check for order of incoming values first
+        """reads serial data from an arduino byte stream, each sensor sends it 4Byte Float after another
+           performs task only ones
+           returns array of length num_sensors as type(float)"""
+        while True:
+            signal = []
+            read = True
+            while read:
+                signal.append(self.Serial_connection.read(4))  # reads four bytes (= float) from the serial stream, add each value to signal
+                if signal.__len__() >= self.num_sensors:  # waits until each package (= each Sensor) is read
+
+                    # loops over all bytes in signal and converts them into float -> returns vector
+                    sensor_values = [struct.unpack("f", bytes(signal[x]))[0] for x in range(0, self.num_sensors, 1)]
+
+                    signal.clear()  # clears signal vector after converting
+                    read = False  # stop reading from serial Port, until called again
+                    testdata = "Hi "
+                    self.data_pipe.send(testdata)   # returns Value vector of size number_of_packages type float
+
 
 
 
--- Source Code/EKG Monitor/main.py
+++ Source Code/EKG Monitor/main.py
@@ -3,15 +3,18 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from datetime import datetime
 from GUI.updateGUI import*
-from Backend.Sensor_read import Sensor
+from Backend.Sensor_read import SensorBoard
 from multiprocessing import Process, Pipe
 
 os.system("pyuic5 -x GUI/ECG_GUI_QTDesigner.ui -o GUI/ECG_GUI_QTDesigner.py")
+SensorBoard = SensorBoard("ESP32", "COM3", 115200, 1)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)  # defines QT Application as app
     MainWindow = QtWidgets.QMainWindow()  # generates main Window
-    ui = Ui_Build(MainWindow)  # Builds GUI from updateGUI
+    ui = Ui_Build(MainWindow, SensorBoard)  # Builds GUI from updateGUI
+    SensorBoard.print_properties()
     MainWindow.show()  # shows App Window
     sys.exit(app.exec_())  # if window closed, stop program
 
